863d1402456354ea56b2ada85c205c30d300c663
apmacias92 <apmacias92@gmail.com>
2021-09-06 17:37:39 -0500

    Small update on Js

--- src/app.js
+++ src/app.js
@@ -44,7 +44,6 @@ function displayForecast(response) {
                   <div class="weather-forecast-date">${formatDay(
                     forecastDay.dt
                   )}</div>
-                  ${index}
                   <img
                     src="http://openweathermap.org/img/wn/${
                       forecastDay.weather[0].icon
